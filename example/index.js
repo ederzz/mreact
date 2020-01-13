6fe0aec59ab35511f14585c37666073449082292
shenyiling <jzx19951028yyjy@163.com>
2020-01-13 17:01:33 +0800

    feat: createElment and render function.

---
+++ example/index.js
@@ -0,0 +1,9 @@
+import render from '../src/render'
+import createElement from '../src/createElement'
+
+/** @jsx createElement */
+
+const element = <h1 title="foo">Hello World!</h1>
+const container = document.getElementById("root")
+
+render(element, container)
